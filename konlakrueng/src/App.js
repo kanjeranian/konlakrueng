663bbfa54172ed736dc032408568de909eaa95e8
kanjeranian <oilscius@gmail.com>
2020-11-20 03:45:29 +0700

    start responsive

--- konlakrueng/src/App.js
+++ konlakrueng/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './img/halfhalf-logo.png'
+import logoMini from './img/halfhalf-logo-mini.png'
+import './App.scss';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="whiteHeader contentContainer">
+        <div className="whiteHeaderLeft">
+          <a href="https://search-merchant.xn--42caj4e6bk1f5b1j.com/">
+            <img src={logo} className="logo" alt="logo" />
+            <img src={logoMini} className="logoMini" alt="logo" />
+          </a>
+        </div>
+        <div className="whiteHeaderRight">whiteHeaderRight</div>
+      </div>
+      <div className="blueHeader">blueHeader</div>
+      <div className="body">
+        <div className="textSearchResult">textSearchResult</div>
+        <div className="leftBox">leftBox</div>
+        <div className="rightBox">rightBox</div>
+      </div>
     </div>
   );
 }
