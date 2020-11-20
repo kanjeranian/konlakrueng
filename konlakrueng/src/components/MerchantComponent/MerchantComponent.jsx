23ab0a1aa202764c14b85bc1c17aa0da56dbe133
kanjeranian <oilscius@gmail.com>
2020-11-21 02:14:08 +0700

    update card

--- konlakrueng/src/components/MerchantComponent/MerchantComponent.jsx
+++ konlakrueng/src/components/MerchantComponent/MerchantComponent.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './MerchantComponent.scss';
-import { useEffect, useContext } from 'react';
+import { useContext } from 'react';
 import { StoreContext } from '../../store.jsx';
 
 export const MerchantComponent = () => {
-  const { state, dispatch } = useContext(StoreContext);
+  const { state } = useContext(StoreContext);
   return (
     <div className='rightBox'>
       {state.data.merchants.map((merchant) => (
